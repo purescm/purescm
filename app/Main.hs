360ec09e330e09a18bf0dc53b7264e27e15299dc
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-06 15:49:34 +0000

    Add simple literals purs and corefn files

--- app/Main.hs
+++ app/Main.hs
@@ -2,4 +2,4 @@ module Main where
 import qualified PureScm.CoreFn as CoreFn
 
 main :: IO ()
-main = CoreFn.readJSONFile "resources/corefn.json"
+main = CoreFn.readJSONFile "resources/PureScmTest.Literals.json"
