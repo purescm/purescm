7cc640b15ad1e035c500aeb9a1128c4e11e9e928
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-10 09:40:27 +0000

    Add printer

---
+++ src/Language/PureScript/Scheme/CodeGen/Printer.hs
@@ -0,0 +1,23 @@
+module Language.PureScript.Scheme.CodeGen.Printer where
+
+import Data.Text                              (Text, pack, intercalate)
+import Language.PureScript.Scheme.CodeGen.AST (AST(..))
+
+showT :: Show a => a -> Text
+showT x = pack $ show x
+
+parens :: Text -> Text
+parens x = "(" <> x <> ")"
+
+list :: [Text] -> Text
+list xs = parens $ intercalate " " xs
+
+define :: Text -> Text -> Text
+define name expr = list ["define", name, expr]
+
+emit :: AST -> Text
+emit (IntegerLiteral integer) = showT integer
+emit (Define name expr) = define name (emit expr)
+
+printScheme :: [AST] -> Text
+printScheme xs = intercalate "\n\n" (fmap emit xs)
