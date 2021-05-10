7a83ebe0e357a22ffe387ad32d907eadb15d002f
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-10 08:44:13 +0000

    Add simple pipeline

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,20 @@
 module Main where
-import qualified PureScm.CoreFn as CoreFn
 
-main :: IO ()
-main = CoreFn.readJSONFile "resources/PureScmTest.Literals.json"
+import qualified Data.Aeson                                   as JSON
+import qualified Data.Aeson.Types                             as JSON.T
+import qualified Language.PureScript.CoreFn.FromJSON          as CoreFn
+import           Language.PureScript.CoreFn.Module            (Module)
+import           Language.PureScript.CoreFn.Ann               (Ann)
+import           Language.PureScript.Scheme.CodeGen.Transpile (moduleToScheme)
+
+main = (fmap moduleToScheme readJSON) >>= print
+
+readJSONFile :: FilePath -> IO (Module Ann)
+readJSONFile path = do
+  r <- JSON.decodeFileStrict' path
+  let res = r >>= JSON.T.parseMaybe CoreFn.moduleFromJSON
+  case res of
+    Nothing -> error "Nope"
+    Just (version, module_) -> return module_
+
+readJSON = readJSONFile "resources/PureScmTest.Literals.corefn.json"
