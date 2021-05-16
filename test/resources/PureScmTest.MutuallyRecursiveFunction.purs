0592b692a4f7eb6f64c8032f3da0d902acf2f56a
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-16 10:39:22 +0000

    Add test for recursive top level definitions

---
+++ test/resources/PureScmTest.MutuallyRecursiveFunction.purs
@@ -0,0 +1,13 @@
+module PureScmTest.MutuallyRecursiveFunction where
+
+foo :: Int -> Int -> Int
+foo 0 n = foo 1 n
+foo 1 n = bar 2 n
+foo 2 n = n
+foo _ _ = 0
+
+bar :: Int -> Int -> Int
+bar 0 m = bar 1 m
+bar 1 m = foo 2 m
+bar 2 m = m
+bar _ _ = 0
