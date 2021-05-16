62f809c99f80595ab5e95503f879df0ba0e9e8e1
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-16 09:03:29 +0000

    Update tests to verify multiple VarBinders handling

--- test/resources/PureScmTest.FunctionMultipleMatch.purs
+++ test/resources/PureScmTest.FunctionMultipleMatch.purs
@@ -1,8 +1,12 @@
 module PureScmTest.FunctionMultipleMatch where
 
+import Prelude
+
 foo 0 0 = 0
 foo 0 1 = 1
 foo 1 0 = 10
 foo _ 2 = 2
 foo 3 _ = 30
-foo _ _ = 50
+foo 4 n = n
+foo m 5 = m
+foo m n = m + n
