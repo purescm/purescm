b177f2dc51ba4d8d5d5d393ae8629bbc760ee694
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-24 21:36:05 +0000

    Add ADT test

---
+++ test/resources/PureScmTest.Constructor.purs
@@ -0,0 +1,10 @@
+module PureScmTest.Constructor where
+
+data Foo = Bar Int Int
+         | Baz
+
+match (Bar 1 2)   = 0
+match (Bar 1 _)   = 1
+match (Bar _ 2)   = 2
+match (Bar i1 i2) = i1
+match Baz = 0
