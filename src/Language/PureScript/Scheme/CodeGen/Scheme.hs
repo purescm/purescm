128d017a270feb786ce0eee3359cf38dae07c31f
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-05-24 21:47:04 +0000

    Refactor Scheme functions application

--- src/Language/PureScript/Scheme/CodeGen/Scheme.hs
+++ src/Language/PureScript/Scheme/CodeGen/Scheme.hs
@@ -10,10 +10,22 @@ app :: Text -> [AST] -> AST
 app name args = Application (Identifier name) args
 
 
+-- Scheme symbols --------------------------------------------------------------
+
+t :: AST
+t = Identifier "#t"
+
+
 -- Scheme functions ------------------------------------------------------------
+
+eq :: [AST] -> AST
+eq xs = app "=" xs
 
 eqQ :: AST -> AST -> AST
 eqQ x y = app "eq?" [x, y]
+
+and_ :: [AST] -> AST
+and_ xs = app "and" xs
 
 quote :: AST -> AST
 quote x = app "quote" [x]
