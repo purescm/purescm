3ffccd81808cd0ccbcfcab40f39917e1e24d96a9
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-06-02 13:25:41 +0000

    Make main generate Scheme files

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,23 @@
 module Main where
 
-import           Data.Text.IO                    as T (putStrLn)
-import           Language.PureScript.Scheme.Make      (compile)
+import           Data.Foldable                   (for_)
+import qualified Data.Text.IO                    as Text.IO
+import qualified Turtle                          as Turtle
+import           Language.PureScript.Scheme.Make (compile)
+import           TestUtil                        (corefnFile, schemeFile, toText,
+                                                  findModules, removeSchemeFiles,
+                                                  removeCorefnFiles, buildCorefn)
+
 
 main :: IO ()
-main = compile "resources/PureScmTest.Fib.corefn.json" >>= T.putStrLn
+main = do
+  removeSchemeFiles
+  removeCorefnFiles
+  buildCorefn
+  modules <- findModules
+  for_ modules $ \m -> do
+    let corefn = corefnFile m
+    let schemePath = schemeFile m
+    scheme <- compile $ Turtle.encodeString corefn
+    Text.IO.putStrLn $ "Compiling " <> m <> " to " <> toText schemePath
+    Turtle.liftIO $ Turtle.writeTextFile schemePath scheme
