b9b5b378d523e5602dfe5d6eae9774346ed55acb
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-08-31 17:03:41 +0000

    Emit library

--- src/Language/PureScript/Scheme/Make.hs
+++ src/Language/PureScript/Scheme/Make.hs
@@ -6,13 +6,17 @@ import qualified Data.Aeson.Types                              as JSON.T
 import qualified Language.PureScript.CoreFn.FromJSON           as CoreFn
 import           Language.PureScript.CoreFn.Module             (Module)
 import           Language.PureScript.CoreFn.Ann                (Ann)
-import           Language.PureScript.Scheme.CodeGen.Transpiler (moduleToScheme)
-import           Language.PureScript.Scheme.CodeGen.Printer    (printScheme)
+import           Language.PureScript.Scheme.CodeGen.Transpiler (moduleToLibrary)
+import           Language.PureScript.Scheme.CodeGen.Printer    (printLibrary)
 import           Language.PureScript.Scheme.CodeGen.Optimizer  (runOptimizations)
+import           Language.PureScript.Scheme.CodeGen.Library    (Library(..))
 
 compile :: FilePath -> IO Text
-compile path = (printScheme . runOptimizations . moduleToScheme)
+compile path = (printLibrary . optimize . moduleToLibrary)
            <$> readModuleFromJSON path
+
+optimize :: Library -> Library
+optimize library = library { libraryBody = runOptimizations $ libraryBody library }
 
 readModuleFromJSON :: FilePath -> IO (Module Ann)
 readModuleFromJSON path = do
