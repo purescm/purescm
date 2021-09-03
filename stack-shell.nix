3a0461d03339a3fc014d80fec9450719ac259ca7
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-09-03 20:44:45 +0000

    Use easy-purescript for stack-shell

--- stack-shell.nix
+++ stack-shell.nix
@@ -6,8 +6,24 @@
 { pkgs ? import <nixpkgs> {}
 , ghc }:
 
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "5716cd791c999b3246b4fe173276b42c50afdd8d";
+      sha256 = "1r9lx4xhr42znmwb2x2pzah920klbjbjcivp2f0pnka7djvd2adq";
+    }) {
+    inherit pkgs;
+  };
+in
+
 pkgs.haskell.lib.buildStackProject {
   inherit ghc;
   name = "purescm-stack";
-  buildInputs = [ pkgs.zlib pkgs.spago pkgs.git ];
+  buildInputs = [ pkgs.zlib
+                  pkgs.git
+                  easy-ps.purs-0_14_4
+                  easy-ps.spago
+                ];
 }
