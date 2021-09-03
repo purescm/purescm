f9c1a7d543659c4e0092d7826a37bc1efea44d82
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-09-03 18:19:46 +0000

    Bump purescript to 0.14.4

--- shell.nix
+++ shell.nix
@@ -1,10 +1,24 @@
 #!/usr/bin/env nix-shell
 
-{ pkgs ? import <nixpkgs> {} }:
-
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import
+    (pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "5716cd791c999b3246b4fe173276b42c50afdd8d";
+      sha256 = "1r9lx4xhr42znmwb2x2pzah920klbjbjcivp2f0pnka7djvd2adq";
+    }) {
+    inherit pkgs;
+  };
+in
 pkgs.mkShell {
   name="purescm";
-  buildInputs = [ pkgs.stack ];
+  buildInputs = [
+    pkgs.stack
+    easy-ps.purs-0_14_4
+    easy-ps.spago
+  ];
   shellHook = ''
     alias stack='stack --nix'
   '';
