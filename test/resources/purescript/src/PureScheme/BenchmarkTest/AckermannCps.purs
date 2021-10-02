36142e7cc883e32d72c7e3f7aa0254d3b1a1c793
Tancredi Orlando <tancredi.orlando@gmail.com>
2021-10-02 08:18:25 +0000

    Fix AckermannCps

--- test/resources/purescript/src/PureScheme/BenchmarkTest/AckermannCps.purs
+++ test/resources/purescript/src/PureScheme/BenchmarkTest/AckermannCps.purs
@@ -2,7 +2,11 @@ module PureScheme.BenchmarkTest.AckermannCps where
 
 import Prelude
 
-test :: Int -> Int -> (Int -> Int) -> Int
-test 0 n cont = cont (n + 1)
-test m 0 cont = test (m - 1) 1 cont
-test m n cont = test m (n - 1) (\a -> test (m - 1) a cont)
+test :: Int -> Int -> Int
+test x y = go x y identity
+  where
+    go :: Int -> Int -> (Int -> Int) -> Int
+    go 0 n cont = cont (n + 1)
+    go m 0 cont = go (m - 1) 1 cont
+    go m n cont = go m (n - 1) (\a -> go (m - 1) a cont)
+
