5130d7c73db7ff6a6240e0eacec782a27da44b76
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-03-18 09:42:27 -0500

    Define R6RS-compliant types; pretty-print types

--- src/PureScript/Backend/Chez/Convert.purs
+++ src/PureScript/Backend/Chez/Convert.purs
@@ -4,13 +4,13 @@ import Prelude
 
 import Data.Argonaut as Json
 import Data.Array as Array
+import Data.Array.NonEmpty as NEA
 import Data.Array.NonEmpty as NonEmptyArray
 import Data.Maybe (Maybe(..))
-import Data.Newtype (wrap)
-import Data.Profunctor.Strong (second)
+import Data.Newtype (un, unwrap, wrap)
 import Data.String.CodeUnits as CodeUnits
-import Data.Tuple (Tuple, uncurry)
-import PureScript.Backend.Chez.Syntax (ChezExpr)
+import Data.Tuple (Tuple(..), uncurry)
+import PureScript.Backend.Chez.Syntax (ChezDefinition(..), ChezExport(..), ChezExpr, ChezImport(..), ChezImportSet(..), ChezLibrary)
 import PureScript.Backend.Chez.Syntax as S
 import PureScript.Backend.Optimizer.Convert (BackendModule, BackendBindingGroup)
 import PureScript.Backend.Optimizer.CoreFn (Ident(..), Literal(..), ModuleName(..), Qualified(..))
@@ -22,29 +22,106 @@ type CodegenEnv =
   { currentModule :: ModuleName
   }
 
-codegenModule :: BackendModule -> ChezExpr
+codegenModule :: BackendModule -> ChezLibrary
 codegenModule { name, bindings, exports } =
   let
     codegenEnv :: CodegenEnv
     codegenEnv = { currentModule: name }
 
-    exports' :: Array Ident
-    exports' = Array.fromFoldable exports
+    exports' :: Array ChezExport
+    exports' = map ExportIdentifier $ coerce $ Array.fromFoldable exports
 
-    bindings' :: Array ChezExpr
-    bindings' = Array.concat $ codegenTopLevelBindingGroup codegenEnv <$> bindings
+    definitions :: Array ChezDefinition
+    definitions = Array.concat $
+      codegenTopLevelBindingGroup codegenEnv <$> bindings
   in
-    S.library name exports' bindings'
+    { "#!r6rs": true
+    , "#!chezscheme": true
+    , name:
+        { identifiers: NEA.cons' (coerce name) [ "lib" ]
+        , version: []
+        }
+    , imports:
+        [ ImportSet $ ImportPrefix
+            (ImportLibrary { identifiers: NEA.singleton "chezscheme", version: Nothing })
+            "scm:"
+        ]
+    , exports: exports'
+    , body: { definitions, exprs: [] }
+    }
 
-codegenTopLevelBindingGroup :: CodegenEnv -> BackendBindingGroup Ident NeutralExpr -> Array ChezExpr
+codegenTopLevelBindingGroup
+  :: CodegenEnv -> BackendBindingGroup Ident NeutralExpr -> Array ChezDefinition
 codegenTopLevelBindingGroup codegenEnv { recursive, bindings }
   | recursive, Just bindings' <- NonEmptyArray.fromArray bindings =
-      [ S.Identifier "recursive-top-level-binding-group" ]
-  | otherwise = codegenBindings codegenEnv bindings
+      [ DefineValue "rtlbg" $ S.Identifier "recursive-top-level-binding-group" ]
+  | otherwise =
+      map (codegenTopLevelBinding codegenEnv) bindings
 
-codegenBindings :: CodegenEnv -> Array (Tuple Ident NeutralExpr) -> Array ChezExpr
-codegenBindings codegenEnv = map (coerce $ uncurry S.define) <<< map
-  (second $ codegenExpr codegenEnv)
+codegenTopLevelBinding :: CodegenEnv -> Tuple Ident NeutralExpr -> ChezDefinition
+codegenTopLevelBinding codegenEnv@{ currentModule } (Tuple (Ident ident) expr) = case unwrap expr of
+  Var (Qualified (Just moduleName) (Ident v))
+    | currentModule == moduleName -> DefineValue ident $ S.Identifier v
+    | otherwise -> DefineValue ident $ S.Identifier $ coerce moduleName <> "." <> v
+  Var (Qualified Nothing (Ident v)) ->
+    DefineValue ident $ S.Identifier v
+  Local i l ->
+    DefineValue ident $ S.Identifier $ coerce $ S.toChezIdent i l
+  Lit l ->
+    DefineValue ident $ codegenLiteral codegenEnv l
+  App f p ->
+    DefineValue ident $ S.chezCurriedApplication (codegenExpr codegenEnv f)
+      (codegenExpr codegenEnv <$> p)
+  Abs a e -> do
+    DefineCurriedFunction
+      ident
+      (map (un Ident <<< uncurry S.toChezIdent) $ a)
+      (codegenExpr codegenEnv e)
+
+  UncurriedApp _ _ ->
+    DefineValue ident $ S.Identifier "uncurried-app"
+  UncurriedAbs _ _ ->
+    DefineValue ident $ S.Identifier "uncurried-abs"
+
+  UncurriedEffectApp _ _ ->
+    DefineValue ident $ S.Identifier "uncurried-effect-app"
+  UncurriedEffectAbs _ _ ->
+    DefineValue ident $ S.Identifier "uncurried-effect-ab"
+
+  Accessor _ _ ->
+    DefineValue ident $ S.Identifier "object-accessor"
+  Update _ _ ->
+    DefineValue ident $ S.Identifier "object-update"
+
+  CtorSaturated _ _ _ _ _ ->
+    DefineValue ident $ S.Identifier "ctor-saturated"
+  CtorDef _ _ _ _ ->
+    DefineValue ident $ S.Identifier "ctor-def"
+
+  LetRec _ _ _ ->
+    DefineValue ident $ S.Identifier "let-rec"
+  Let i l v e ->
+    DefineValue ident $ S.chezLet (S.toChezIdent i l) (codegenExpr codegenEnv v)
+      (codegenExpr codegenEnv e)
+  Branch _ _ ->
+    DefineValue ident $ S.Identifier "branch"
+
+  EffectBind _ _ _ _ ->
+    DefineValue ident $ S.Identifier "effect-bind"
+  EffectPure _ ->
+    DefineValue ident $ S.Identifier "effect-pure"
+  EffectDefer _ ->
+    DefineValue ident $ S.Identifier "effect-defer"
+
+  PrimOp _ ->
+    DefineValue ident $ S.Identifier "prim-op"
+  PrimEffect _ ->
+    DefineValue ident $ S.Identifier "prim-effect"
+  PrimUndefined ->
+    DefineValue ident $ S.Identifier "prim-undefined"
+
+  Fail _ ->
+    DefineValue ident $ S.Identifier "fail"
 
 codegenExpr :: CodegenEnv -> NeutralExpr -> ChezExpr
 codegenExpr codegenEnv@{ currentModule } (NeutralExpr s) = case s of
