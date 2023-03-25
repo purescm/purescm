82552222fafc4c80cff7839ce41e803903be79fb
purefunctor <purefunctor@pm.me>
2023-03-25 13:56:11 +0800

    Use MonadState to ensure exports are created

--- src/PureScript/Backend/Chez/Runtime.purs
+++ src/PureScript/Backend/Chez/Runtime.purs
@@ -2,52 +2,67 @@ module PureScript.Backend.Chez.Runtime where
 
 import Prelude
 
+import Control.Monad.State (runState)
+import Control.Monad.State.Class (class MonadState, modify_)
+import Data.Array as Array
 import Data.Array.NonEmpty as NEA
 import Data.Maybe (Maybe(..))
-import PureScript.Backend.Chez.Syntax (ChezDefinition(..), ChezExport(..), ChezImport(..), ChezImportSet(..), ChezLibrary)
+import Data.Traversable (sequence)
+import Data.Tuple (Tuple(..))
+import PureScript.Backend.Chez.Syntax (ChezDefinition(..), ChezExport(..), ChezExpr, ChezImport(..), ChezImportSet(..), ChezLibrary)
 import PureScript.Backend.Chez.Syntax as S
 
+makeExportedFunction
+  :: forall m
+   . Monad m
+  => MonadState (Array ChezExport) m
+  => String
+  -> Array String
+  -> ChezExpr
+  -> m ChezDefinition
+makeExportedFunction name args expr = do
+  modify_ $ Array.cons $ ExportIdentifier name
+  pure $ DefineUncurriedFunction name args expr
+
+makeBooleanComparison
+  :: forall m. Monad m => MonadState (Array ChezExport) m => String -> m ChezDefinition
+makeBooleanComparison suffix =
+  makeExportedFunction ("boolean" <> suffix) [ "x", "y" ] $
+    S.List
+      [ S.Identifier $ "scm:fx" <> suffix
+      , S.List [ S.Identifier "boolean->integer", S.Identifier "x" ]
+      , S.List [ S.Identifier "boolean->integer", S.Identifier "y" ]
+      ]
+
 runtimeModule :: ChezLibrary
-runtimeModule =
+runtimeModule = do
+  let
+    Tuple definitions exports = flip runState [] $ sequence
+      [ pure $ DefineUncurriedFunction "boolean->integer" [ "x" ] $ S.List
+          [ S.Identifier "scm:if"
+          , S.Identifier "x"
+          , S.Integer $ S.LiteralDigit "1"
+          , S.Integer $ S.LiteralDigit "0"
+          ]
+      , makeBooleanComparison ">?"
+      , makeBooleanComparison ">=?"
+      , makeBooleanComparison "<=?"
+      , makeBooleanComparison "<?"
+      ]
   { "#!chezscheme": true
   , "#!r6rs": true
   , name:
       { identifiers: NEA.cons' "_Chez_Runtime" [ "lib" ]
       , version: []
       }
-  , exports:
-      [ ExportIdentifier "boolean>?"
-      , ExportIdentifier "boolean>=?"
-      , ExportIdentifier "boolean<=?"
-      , ExportIdentifier "boolean<?"
-      ]
+  , exports
   , imports:
       [ ImportSet $ ImportPrefix
           (ImportLibrary { identifiers: NEA.singleton "chezscheme", version: Nothing })
           "scm:"
       ]
   , body:
-      { definitions:
-          [ DefineUncurriedFunction "boolean->integer" [ "x" ] $ S.List
-              [ S.Identifier "scm:if"
-              , S.Identifier "x"
-              , S.Integer $ S.LiteralDigit "1"
-              , S.Integer $ S.LiteralDigit "0"
-              ]
-          , makeBooleanComparison ">?"
-          , makeBooleanComparison ">=?"
-          , makeBooleanComparison "<=?"
-          , makeBooleanComparison "<?"
-          ]
+      { definitions
       , expressions: []
       }
   }
-
-makeBooleanComparison :: String -> ChezDefinition
-makeBooleanComparison suffix =
-  DefineUncurriedFunction ("boolean" <> suffix) [ "x", "y" ] $
-    S.List
-      [ S.Identifier $ "scm:fx" <> suffix
-      , S.List [ S.Identifier "boolean->integer", S.Identifier "x" ]
-      , S.List [ S.Identifier "boolean->integer", S.Identifier "y" ]
-      ]
