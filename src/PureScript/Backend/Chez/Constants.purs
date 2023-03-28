cb87621f8851045aefea12a948123e7c8ad30d92
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-03-28 10:56:08 -0500

    Add constants for library prefixes

---
+++ src/PureScript/Backend/Chez/Constants.purs
@@ -0,0 +1,15 @@
+module PureScript.Backend.Chez.Constants where
+
+import Prelude
+
+libChezSchemePrefix :: String
+libChezSchemePrefix = "scm:"
+
+runtimePrefix :: String
+runtimePrefix = "rt:"
+
+scmPrefixed :: String -> String
+scmPrefixed = append libChezSchemePrefix
+
+rtPrefixed :: String -> String
+rtPrefixed = append runtimePrefix
