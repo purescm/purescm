f649193cec64e175b9aaeea6f90d02a9c4f2efc0
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2023-03-29 11:53:41 +0300

    Pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
-{ pkgs ? import <nixpkgs> { } }:
 let
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/402cc3633cc60dfc50378197305c984518b30773.tar.gz";
+    sha256 = "0yvlprdkqg1kizg83j7nivlc58zk7llrbf82jqvgjimrwhsva1m9";
+  };
+
+  pkgs = import nixpkgs {};
+
   easy-ps = import
     # On f-f's fork of easy-purescript-nix, as spago-next is not in mainline yet
     (pkgs.fetchFromGitHub {
