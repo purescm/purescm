d4d096c249224b830b4fdaf165694351b111240a
purefunctor <purefunctor@pm.me>
2023-04-04 23:53:15 +0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Chez Scheme backend for PureScript";
+
+  inputs = {
+    nixpkgs.url =
+      "github:nixos/nixpkgs?rev=402cc3633cc60dfc50378197305c984518b30773";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    easy-purescript-nix = {
+      url = "github:f-f/easy-purescript-nix";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, easy-purescript-nix, ... }:
+    let supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+    in flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        easy-ps = pkgs.callPackage easy-purescript-nix { };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            name = "purescm";
+            packages = [
+              easy-ps.purs-0_15_8
+              easy-ps.purs-tidy
+              easy-ps.psa
+              easy-ps.spago-next
+              easy-ps.purescript-language-server
+              pkgs.nodejs-slim-16_x
+              pkgs.chez-racket
+            ];
+          };
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      });
+}
--- shell.nix
+++ shell.nix
@@ -1,29 +1,12 @@
-let
-  nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/402cc3633cc60dfc50378197305c984518b30773.tar.gz";
-    sha256 = "0yvlprdkqg1kizg83j7nivlc58zk7llrbf82jqvgjimrwhsva1m9";
-  };
-
-  pkgs = import nixpkgs {};
-
-  easy-ps = import
-    # On f-f's fork of easy-purescript-nix, as spago-next is not in mainline yet
-    (pkgs.fetchFromGitHub {
-      owner = "f-f";
-      repo = "easy-purescript-nix";
-      rev = "b02cff3db1671fc8fb76a680597a216a9c9b2d03";
-      sha256 = "sha256-vfzrVwBntXao3nMi2hkjYlWGUnuyUOVmYi95mQQ0EEY=";
-    }) {
-    inherit pkgs;
-  };
-in
-pkgs.mkShell {
-  buildInputs = [
-    easy-ps.purs-0_15_8
-    easy-ps.purs-tidy
-    easy-ps.psa
-    easy-ps.spago-next
-    pkgs.nodejs-slim-16_x
-    pkgs.chez-racket
-  ];
-}
+# A compatibility file that allows non-flakes users to still get a development
+# shell with `nix-shell`.
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in fetchTarball {
+      url =
+        "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }).shellNix
