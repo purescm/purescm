e99d6234b753b9139cba1d8d6345cf5c42eaea1c
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2024-10-08 11:07:11 +0100

    Add shim for testing the runtime with the right dylibs

---
+++ test-runtime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { spawnSync } from 'child_process';
+let env = process.env;
+env['DYLD_LIBRARY_PATH'] = env['CHEZ_DYLD_LIBRARY_PATH'];
+spawnSync('scheme', ["--libdirs", "vendor:", "--program", "test-runtime.ss"], { env, stdio: 'inherit' });
